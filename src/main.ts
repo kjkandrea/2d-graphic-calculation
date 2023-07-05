ff17b79b5f63f23f53440739458a13241cbd0140
kjkandrea <kjkandrea@gmail.com>
2023-07-05 11:37:01 +0900

    feat: canvas 보일러플레이트 코드 작성

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import './style.css';
+
+const canvas = document.getElementById('graphic-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+const { height } = canvas;
+
+// 이 코드가 왜 필요한지 아직 이해하지 못함
+ctx.translate(0, height);
+ctx.scale(-1, 1);
