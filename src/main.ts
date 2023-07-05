b7939153b22e5840981d52a1c12a07e7e79d90ab
kjkandrea <kjkandrea@gmail.com>
2023-07-05 12:46:17 +0900

    refactor

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import './style.css';
 const canvas = document.getElementById('graphic-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-(function 격자그리기() {
+draw_grid();
+
+function draw_grid() {
   const grid = 25;
 
   ctx.scale(grid, grid);
@@ -19,7 +21,6 @@ const ctx = canvas.getContext('2d');
   draw_grid();
 
   function draw_grid() {
-    console.log('draw_grid');
     ctx.setLineDash([0.1, 0.1]); // 파선
     ctx.strokeStyle = 'rgb(128, 128, 128)'; // 회색
     ctx.beginPath();
@@ -39,7 +40,7 @@ const ctx = canvas.getContext('2d');
     ctx.stroke();
     ctx.restore();
   }
-})();
+}
 
 function clear() {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // w 직사각형 영역* h 의 픽셀을 모두 clear
